4d914821f7efa41df653677073e6ca517d29e35f
ZdravkoRistic <easycodemaster@gmail.com>
2024-01-15 10:20:01 +0100

    text optimization

--- constants/index.ts
+++ constants/index.ts
@@ -30,11 +30,11 @@ export const FEATURES = [
       "U trci za visokim rangiranjem na pretraživačima, naša agencija je vaš ključ ka optimizaciji. Sa fokusom na SEO strategijama, analizi ključnih reči i tehničkoj optimizaciji, obezbeđujemo da vaša web stranica bude u vrhu rezultata pretrage. Postignite bolju vidljivost, povećajte organski saobraćaj i izgradite održivu online prisutnost uz naše SEO rešenje.",
   },
   {
-    title: "Izrada Web Sajta",
+    title: "Izrada Web Sajtova",
     icon: "/web.svg",
     variant: "blue",
     description:
-      "Mi kreiramo veb sajtove kodiranjem od nule. Vaša web stranica je ključna komponenta vašeg poslovanja i marketinške strategije. To je onlajn segment vašeg brenda i pruža virtuelnu prezentaciju vašeg poslovanja za sve posetioce! Pravimo web-sajtove prilagođene mobilnim uređajima, omogućavajući našim klijentima da budu prisutni na svim uređajima, zadržavajući potpunu funkcionalnost i estetiku!",
+      "Mi kreiramo web sajtove kodiranjem od nule. Vaša web stranica je ključna komponenta vašeg poslovanja i marketinške strategije. To je onlajn segment vašeg brenda i pruža virtuelnu prezentaciju vašeg poslovanja za sve posetioce! Pravimo web-sajtove prilagođene mobilnim uređajima, omogućavajući našim klijentima da budu prisutni na svim uređajima, zadržavajući potpunu funkcionalnost i estetiku!",
   },
 ];
 
